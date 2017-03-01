c1583ccf3ff5efa7bea97ce4e8f9472b0ef529e0
stealpattan <stealpattan@gmail.com>
2017-03-01 13:13:05 +0900

    arrange manager's page

--- ccp/manager.php
+++ ccp/manager.php
@@ -12,14 +12,25 @@
 	    <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
 	    <link rel="stylesheet" href="css/common.css">
 	    <link rel="stylesheet" href="css/home.css">
+	    <link rel="stylesheet" type="text/css" href="css/intern.css">
 	    <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
 	</head>
 	<body>
 		<?php require("header.php"); ?>
-			<div width='70%' height='60px'>
-				<h2>シルモク管理用ページ</h2>
-				<p>シルモクに関する変更、情報の閲覧を行います</p>
-			</div>
+		<div id="manager_page">
+			<a href="" style='text-decoration:none'>
+				<div class='manager' width='70%' height='60px'>
+					<h2>シルモク管理用ページ</h2>
+					<p>シルモクに関する変更、情報の閲覧を行います</p>
+				</div>
+			</a>
+			<a href="" style='text-decoration:none;'>
+				<div class='manager' width='70%' height='60px'>
+					<h2>新着情報更新ページ</h2>
+					<p>様々な新着情報の更新を行います</p>
+				</div>
+			</a>
+		</div>
 		<?php require("footer.php"); ?>
 	</body>
 </html>
