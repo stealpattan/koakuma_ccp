3c9b124bd4d0aec11dc002a298fe13108dc1b8b1
stealpattan <stealpattan@gmail.com>
2017-03-01 12:48:31 +0900

    add header and original manager's page

---
+++ ccp/header.php
@@ -0,0 +1,11 @@
+<header>
+  <img class="logo" src="img/logo/tpu_logo_set.svg" alt="TPUのロゴ"/>
+  <!-- ナビメニュー -->
+  <div class="nav-menu">
+    <ul id="menu">
+      <li id="home"><a class="selected_tab" href="home.php">ホーム</a></li>
+      <li id="info-career"><a class="unselected_tab" href="info_career.php">就職情報</a></li>
+      <li id="intern"><a class="unselected_tab" href="recruitment.php">求人情報</a></li>
+    </ul>
+  </div>
+</header>
---
+++ ccp/manager.php
@@ -0,0 +1,25 @@
+<?php 
+	
+?>
+
+<!DOCTYPE html>
+<html lang='ja'>
+	<head>
+		<meta charset='utf8'>
+		<title>管理者ページ</title>
+		<link rel="shortcut icon" href="img/logo/tpu_logo.png">
+	    <link rel="stylesheet" href="css/reset.css">
+	    <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
+	    <link rel="stylesheet" href="css/common.css">
+	    <link rel="stylesheet" href="css/home.css">
+	    <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+	</head>
+	<body>
+		<?php require("header.php"); ?>
+			<div width='70%' height='60px'>
+				<h2>シルモク管理用ページ</h2>
+				<p>シルモクに関する変更、情報の閲覧を行います</p>
+			</div>
+		<?php require("footer.php"); ?>
+	</body>
+</html>
