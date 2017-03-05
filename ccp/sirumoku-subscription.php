2c58054ce0b5011df2d16bb140838f057d6a12ed
AnQ <mskz625@gmail.com>
2017-03-06 01:00:50 +0800

    シルモク申し込みフォームの作成

--- ccp/sirumoku-subscription.php
+++ ccp/sirumoku-subscription.php
@@ -1,17 +1,8 @@
 <?php
   require('dbconnect.php');
-
-  $data=$_POST['date'];
-  $student_name_data=$_POST['name'];
-  $student_number_data=$_POST['student_number'];
-
-  $sql_data=sprintf('SELECT * FROM `sirumoku_data` WHERE sirumoku_data.date="%s"', $data);
-  $record_data=mysqli_query($db,$sql_data);
-  $table_data=mysqli_fetch_assoc($record_data);
-
-  $sql_student_data=sprintf('SELECT * FROM sirumoku_student_data, prefectures WHERE sirumoku_student_data.prefecture_id = prefectures.prefecture_id AND sirumoku_student_data.student_number="%d"', $student_number_data);
-  $record_student_data=mysqli_query($db,$sql_student_data);
-  $table_student_data=mysqli_fetch_assoc($record_student_data);
+  $sql_date=sprintf('SELECT * FROM `sirumoku_data` WHERE 1');
+  $record_date=mysqli_query($db,$sql_date);
+  $record_data=mysqli_query($db,$sql_date);
 
   $date = date('Y-m-d');
   $deadline=date('Y-m-d', strtotime("+3 day"));
@@ -25,7 +16,9 @@
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/home.css">
     <link rel="stylesheet" href="css/progress_bar.css">
+    <link rel="stylesheet" href="css/sirumoku.css">
     <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
   </head>
   <body>
@@ -42,50 +35,75 @@
       <div class="clear"></div>
     </header>
     <div class="contents">
-      <div class="progress check">
+      <div class="progress sub">
         <div class="one one-success-color"></div><div class="two two-success-color"></div><div class="three three-success-color"></div>
-  			<div class="progress-bar progress-bar-success" style="width: 50%"></div>
+  			<div class="progress-bar progress-bar-success" style="width: 0%"></div>
 		  </div>
-      <?php
+      <div class="sirumoku_datas">
+        <table class="table table-bordered table-striped trhover">
+          <tr class="s_data_list">
+            <th class="s_data_day">開催日</th>
+            <th class="s_data_time">時間</th>
+            <th class="s_data_name">企業名</th>
+          </tr>
+          <?php
+          while($table_date=mysqli_fetch_assoc($record_date)){
+            if($table_date['date'] > $deadline){
+              $array = explode("-", $table_date['date']);
+              $str1 = str_split($array[1]);
+              $str2 = str_split($array[2]);
+              for ($i=0; $i < 2; $i++) {
+                if($str1[$i] == 0){
+                  $str1[$i] = '';
+                }
+                if($str2[$i] == 0){
+                  $str2[$i] = '';
+                }
+                if($i==1){
+                  $str1=$str1[$i-1].$str1[$i];
+                  $str2=$str2[$i-1].$str2[$i];
+                  $date_time=$array[0]."/".$str1."/".$str2;
+                }
+              }
+              $table_st_data=htmlspecialchars($table_date['start-time']);
+              $array = explode(":", $table_st_data);
+              $data_start=$array[0].":".$array[1];
 
-      $table_date_data=htmlspecialchars($table_data['date']);
-      $array = explode("-", $table_date_data);
-      $str1 = str_split($array[1]);
-      $str2 = str_split($array[2]);
-      for ($i=0; $i < 2; $i++) {
-        if($str1[$i] == 0){
-          $str1[$i] = '';
-        }
-        if($str2[$i] == 0){
-          $str2[$i] = '';
-        }
-        if($i==1){
-          $str1=$str1[$i-1].$str1[$i];
-          $str2=$str2[$i-1].$str2[$i];
-          echo $array[0]."/".$str1."/".$str2."<br>";
-        }
-      }
-      //echo htmlspecialchars($table_data['date']);
-      $table_st_data=htmlspecialchars($table_data['start-time']);
-      $array = explode(":", $table_st_data);
-      echo $array[0].":".$array[1]."<br>";
-      // echo htmlspecialchars($table_data['start-time']);
-      $table_ft_data=htmlspecialchars($table_data['finish-time']);
-      $array = explode(":", $table_ft_data);
-      echo $array[0].":".$array[1]."<br>";
-      // echo htmlspecialchars($table_data['finish-time']);
-      echo htmlspecialchars($table_data['place'])."<br>";
-      $table_company_data=htmlspecialchars($table_data['name_company']);
-      $array = explode(",", $table_company_data);
-      echo $array[0]." ".$array[1]."<br>";
+              $table_ft_data=htmlspecialchars($table_date['finish-time']);
+              $array = explode(":", $table_ft_data);
+              $data_finish=$array[0].":".$array[1];
 
-      echo htmlspecialchars($table_student_data['student_name'])."<br>";
-      echo htmlspecialchars($table_student_data['student_number'])."<br>";
-      echo htmlspecialchars($table_student_data['sex'])."<br>";
-      echo htmlspecialchars($table_student_data['mail'])."<br>";
-      echo htmlspecialchars($table_student_data['prefecture_name'])."<br>";
-      ?>
-
+              $table_company_data=htmlspecialchars($table_date['name_company']);
+              $array = explode(",", $table_company_data);
+          ?>
+              <tr>
+                <th class="table_data_date"><?php echo htmlspecialchars($date_time); ?></th>
+                <th class="table_data_time"><?php echo htmlspecialchars($data_start."~".$data_finish); ?></th>
+                <th><?php echo htmlspecialchars($array[0])."<br>".htmlspecialchars($array[1]); ?></th>
+              </tr>
+          <?php
+            }
+          }
+          ?>
+        </table>
+      </div>
+      <form id="form" action="sirumoku-subscription-check.php" method="post">
+        <select class="" name="date">
+          <option value="none">選択してください</option>
+          <?php
+          while($table_data=mysqli_fetch_assoc($record_data)){
+            if($table_data['date'] > $deadline){
+          ?>
+              <option name="date" value="<?php print(htmlspecialchars($table_data['date'])); ?>"><?php print(htmlspecialchars($table_data['date'])); ?></option>
+          <?php
+            }
+          }
+          ?>
+        </select>
+        <input type="text" name="name" placeholder="氏名">
+        <input type="text" name="student_number" placeholder="学籍番号">
+        <input type="submit" name="" value="参加申込する">
+      </form>
     </div>
     <?php include('footer.php'); ?>
   </body>
