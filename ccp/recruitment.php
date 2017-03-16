3f8f827834958ba42bb5b18c75f509ddf1ae704b
stealpattan <stealpattan@gmail.com>
2017-03-16 18:03:33 +0900

    repair head picture

--- ccp/recruitment.php
+++ ccp/recruitment.php
@@ -142,21 +142,7 @@ while($table = mysqli_fetch_assoc($recordSet)){
     </script>
   </head>
   <body>
-    <header>
-      <div class="header-logo">
-        <a href="home.php"><img class="logo" src="img/logo/tpu_logo_set.svg" alt="TPUのロゴ"/></a>
-      </div>
-      <!-- ナビメニュー -->
-      <div class="nav-menu">
-        <ul id="menu">
-          <li id="home"><a class="unselected_tab" href="home.php">ホーム</a></li>
-          <li id="info-career"><a class="unselected_tab" href="info_career.php">就職情報</a></li>
-          <li id="intern"><a class="selected_tab" href="recruitment.php">求人情報</a></li>
-        </ul>
-      </div>
-      <div class="clear"></div>
-    </header>
-
+    <?php require('header.php'); ?>
     <?php if(ip_tracer() == true): ?>
       <div class="searchform">
         <form class="searchform_re" action="recruitment.php#result" method="post">
