9a89c4e14d1baead48c827a6ab2d4673a250ddf8
stealpattan <stealpattan@gmail.com>
2017-03-18 18:33:33 +0900

    change style...header

--- ccp/header.php
+++ ccp/header.php
@@ -2,10 +2,10 @@
   <img class="logo" width='100%' src="img/logo/tpu_logo_set.svg" alt="TPUのロゴ"/>
   <!-- ナビメニュー -->
   <div class="nav-menu">
-    <ul id="menu">
-      <li id="home"><a class="selected_tab" href="home.php">ホーム</a></li>
-      <li id="info-career"><a class="unselected_tab" href="info_career.php">就職情報</a></li>
-      <li id="intern"><a class="unselected_tab" href="recruitment.php">求人情報</a></li>
+    <ul>
+      <a class="<?php if(basename($_SERVER['PHP_SELF']) == 'home.php'){echo 'selected_tab';} ?>" href="home.php"><div class='header_style' id="home">ホーム</div></a>
+      <a class="<?php if(basename($_SERVER['PHP_SELF']) == 'info_career.php'){echo 'selected_tab';} ?>" href="info_career.php"><div class='header_style' id="info-career">就職情報</div></a>
+      <a class="<?php if(basename($_SERVER['PHP_SELF']) == 'recruitment.php'){echo 'selected_tab';} ?>" href="recruitment.php"><div class='header_style' id="intern">求人情報</div>
     </ul>
   </div>
 </header>
