eaee3d297a5811312cdf1e7fb13940922bd5ed39
stealpattan <stealpattan@gmail.com>
2017-03-20 16:29:58 +0900

    repair little

--- ccp/header.php
+++ ccp/header.php
@@ -3,9 +3,9 @@
   <!-- ナビメニュー -->
   <div class="nav-menu">
     <ul>
-      <a class="<?php if(basename($_SERVER['PHP_SELF']) == 'home.php'){echo 'selected_tab';} ?>" href="home.php"><div class='header_style' id="home">ホーム</div></a>
-      <a class="<?php if(basename($_SERVER['PHP_SELF']) == 'info_career.php'){echo 'selected_tab';} ?>" href="info_career.php"><div class='header_style' id="info-career">就職情報</div></a>
-      <a class="<?php if(basename($_SERVER['PHP_SELF']) == 'recruitment.php'){echo 'selected_tab';} ?>" href="recruitment.php"><div class='header_style' id="intern">求人情報</div>
+      <a class="<?php if(basename($_SERVER['PHP_SELF']) == 'home.php'){echo 'selected_tab';}else{echo "unselected_tab";} ?>" href="home.php"><div class='header_style' id="home">ホーム</div></a>
+      <a class="<?php if(basename($_SERVER['PHP_SELF']) == 'info_career.php'){echo 'selected_tab';}else{echo "unselected_tab";} ?>" href="info_career.php"><div class='header_style' id="info-career">就職情報</div></a>
+      <a class="<?php if(basename($_SERVER['PHP_SELF']) == 'recruitment.php'){echo 'selected_tab';}else{echo "unselected_tab";} ?>" href="recruitment.php"><div class='header_style' id="intern">求人情報</div></a>
     </ul>
   </div>
 </header>
