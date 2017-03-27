3ad07cf5587d384bc645916ea3f3c4cd71a2c2c3
stealpattan <stealpattan@gmail.com>
2017-03-27 13:22:53 +0900

    add link for event that is shown in calendar

--- ccp/home.php
+++ ccp/home.php
@@ -157,7 +157,7 @@ $deadline=date('Y-m-d', strtotime("+3 day"));
                     echo "<br>";
                     foreach($_SESSION['cal_event'] as $cal_event){
                       if($cal_event['day'] == $calendar[$i]['day']){
-                        echo "<a style='text-decoration:none;' href=''>";
+                        echo "<a style='text-decoration:none;' href='allnews.php'>";
                         echo $cal_event['title'];
                         echo "</a>";
                       }
