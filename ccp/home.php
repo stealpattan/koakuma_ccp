542b22e8fba84cbf968e34d7483000b97a7a87ae
stealpattan <stealpattan@gmail.com>
2017-04-06 13:22:32 +0900

    small repair

--- ccp/home.php
+++ ccp/home.php
@@ -231,7 +231,7 @@ function check_limit($str){
             <?php foreach($sirumoku_table as $st): ?>
               <tr style='text-align:center;'>
                 <?php
-                  $jp_day = array("Monday" => '月', "Tuesday" => "火", "Wednesday" => "水", "Thirsday" => "木", "Friday" => "金");
+                  $jp_day = array("Monday" => '月', "Tuesday" => "火", "Wednesday" => "水", "Thursday" => "木", "Friday" => "金");
                   $d = explode("-" , $st['date']);
                   $d = (int)$d[0] . "年" . (int)$d[1] . "月" . (int)$d[2] . "日" . "(" . $jp_day[date('l', mktime(0,0,0,(int)$d[0],(int)$d[1],(int)$d[2]))] . ")";
                 ?>
