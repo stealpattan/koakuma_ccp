df1b341bffa7cd095736068703ee97fce68ed46a
Masakazu Ankyu <mskz625@gmail.com>
2017-06-07 21:23:31 +0900

    change code for header

--- ccp/header.php
+++ ccp/header.php
@@ -1,11 +1,11 @@
 <header>
-  <img class="logo" width='100%' src="./assets/img/logo/tpu_logo_set.svg" alt="TPUのロゴ"/>
+  <img class="nav-logo" width='100%' src="./assets/img/logo/tpu_logo_set.svg" alt="TPUのロゴ"/>
   <!-- ナビメニュー -->
   <div class="nav-menu">
     <ul>
-      <a class="<?php if(basename($_SERVER['PHP_SELF']) == 'home.php'){echo 'selected_tab';}else{echo "unselected_tab";} ?>" href="home.php"><div class='header_style' id="home">ホーム</div></a>
-      <a class="<?php if(basename($_SERVER['PHP_SELF']) == 'info_career.php'){echo 'selected_tab';}else{echo "unselected_tab";} ?>" href="info_career.php"><div class='header_style' id="info-career">就職情報</div></a>
-      <a class="<?php if(basename($_SERVER['PHP_SELF']) == 'recruitment.php'){echo 'selected_tab';}else{echo "unselected_tab";} ?>" href="recruitment.php"><div class='header_style' id="intern">求人情報</div></a>
+      <li class="<?php if(basename($_SERVER['PHP_SELF']) == 'home.php'){echo 'selected_tab';}else{echo "unselected_tab";} ?>"><a href="home.php">ホーム</a></li>
+      <li class="<?php if(basename($_SERVER['PHP_SELF']) == 'info_career.php'){echo 'selected_tab';}else{echo "unselected_tab";} ?>"><a href="info_career.php">就職情報</a></li>
+      <li class="<?php if(basename($_SERVER['PHP_SELF']) == 'recruitment.php'){echo 'selected_tab';}else{echo "unselected_tab";} ?>"><a href="recruitment.php">求人情報</a></li>
     </ul>
   </div>
 </header>
